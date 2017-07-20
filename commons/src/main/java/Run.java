dad90a3edec2e5808d3c97fc311a0456a09f337b
dheerajkhatri <er.dheerajkhatri@gmail.com>
2017-07-20 16:46:40 +0530

    basic udf to get data from hbase

--- commons/src/main/java/Run.java
+++ commons/src/main/java/Run.java
@@ -1,16 +1,47 @@
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.io.IOException;
 
 /**
- * Created by dheeraj.khatri on 14/06/17.
+ * Created by dheeraj.khatri on 20/07/17.
  */
-public class Run {
+public class Run extends UDF {
+    private static HTable hTable;
 
-    public static void main(String args[]) throws Exception {
-        System.out.println("Starting Project...");
-        final Injector injector = Guice.createInjector(new ServiceModule());
-        final ServiceApplication application = injector.getInstance(ServiceApplication.class);
+    public static void main(String[] args) throws IOException {
+        Run hit = new Run();
+        String result = hit.evaluate(args[0]);
+        System.out.println(result);
+    }
 
-        application.run(new String[] {"server", "configuration.yaml"});
+    public Run() throws IOException {
+        System.setProperty("hadoop.home.dir", "/");
+
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.property.clientPort", "2181");
+        conf.set("hbase.zookeeper.quorum", "10.33.14.133,10.33.18.132,10.33.5.184");
+        conf.set("zookeeper.znode.parent", "/hbase-unsecure");
+        hTable = new HTable(conf,"concat_ht_dk");
+    }
+
+    public String evaluate(String fsn) throws IOException {
+        if (fsn.length() == 0 || fsn == null)
+            return null;
+
+        String sc = "";
+        try {
+            Get g = new Get(Bytes.toBytes(fsn));
+            Result result = hTable.get(g);
+            byte[] value = result.getValue(Bytes.toBytes("d"),Bytes.toBytes("sc"));
+            return Bytes.toString(value);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
